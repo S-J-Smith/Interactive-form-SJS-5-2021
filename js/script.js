8faabae005c6075ad99576c09a22ef53662fe7cf
Steve Smith <58996112+S-J-Smith@users.noreply.github.com>
2021-06-06 10:23:33 +0100

    Assigned variable to Job Role and created addEventListener
    Stuck on Hiding and showing the Other job role text box.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-document.querySelector('#name').focus();
-document.querySelector('#other-job-role').hidden = true;
+const focusOnName = document.querySelector('#name').focus();
+const hideOtherJobRole = document.querySelector('#other-job-role').hidden = true;
+const selectJobRole = document.querySelector('#title');
+const JobRoleOptions = document.querySelectorAll('#title option');
+
+//console.log(JobRoleOptions[6]);
+
+selectJobRole.addEventListener('change', e => {
+    console.log(e.target.value);
+    for(let i = 0; i <= JobRoleOptions.length; i++) {
+        const role = JobRoleOptions[i];
+        // console.log(role);
+            if(role === e.target.value) {
+                
+            }
+     }
+ });
