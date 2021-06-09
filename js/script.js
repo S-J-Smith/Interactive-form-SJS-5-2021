251c9131838fb716998adb1a9efaa40dc13fee5a
Steve Smith <58996112+S-J-Smith@users.noreply.github.com>
2021-06-09 16:44:32 +0100

    Updated to fix issue with event listener
    Event listener now unhides the text box for Other Job Role when Other is selected from the Job role drop down.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
+//Assigns variables for relevant elements on the page.
 const focusOnName = document.querySelector('#name').focus();
-const hideOtherJobRole = document.querySelector('#other-job-role').hidden = true;
+const otherJobRole = document.querySelector('#other-job-role');
 const selectJobRole = document.querySelector('#title');
 const JobRoleOptions = document.querySelectorAll('#title option');
-
-//console.log(JobRoleOptions[6]);
-
+//Hides the other job role text box.
+otherJobRole.hidden = true;
+//Listens for when the Job Role dropdown box is changed and an option is selected.
 selectJobRole.addEventListener('change', e => {
+    //Logs out to the console the value of the targeted event.
     console.log(e.target.value);
-    for(let i = 0; i <= JobRoleOptions.length; i++) {
-        const role = JobRoleOptions[i];
-        // console.log(role);
-            if(role === e.target.value) {
-                
-            }
-     }
+        //If the chosen option is equal to other then unhide the other job role text box otherwise keep it hidden.
+        if(e.target.value === 'other') {
+            otherJobRole.hidden = false;
+        } else {
+            otherJobRole.hidden = true;
+        }
  });
